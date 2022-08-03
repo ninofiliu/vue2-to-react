8063437b3101e74410dce9f45e85a98b09dff2e8
Nino Filiu <nino.filiu@gmail.com>
2022-08-03 16:55:20 +0200

    setup test architecture

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -1,7 +1,24 @@
+import { resolve } from "path";
+import { readdirSync } from "fs";
+import { readFile } from "fs/promises";
+import { parse } from "@typescript-eslint/typescript-estree";
 import lib from "./lib";
 
-describe("lib", () => {
-  it("should work", () => {
-    expect(lib()).toEqual("it works");
+const dir = readdirSync(resolve(__dirname, "../fixtures"));
+
+for (const tsxPath of dir.filter((name) => name.endsWith(".tsx"))) {
+  const testName = /^[^\.]*/.exec(tsxPath)[0];
+  it(testName, async () => {
+    const tsxStr = await readFile(
+      resolve(__dirname, "../fixtures", tsxPath),
+      "utf8"
+    );
+    const tsxAst = parse(tsxStr, { jsx: true });
+    const vuePath = tsxPath.replace(/\.tsx$/, ".vue");
+    const vueStr = await readFile(
+      resolve(__dirname, "../fixtures", vuePath),
+      "utf8"
+    );
+    expect(lib(vueStr)).toEqual(tsxAst);
   });
-});
+}
--- src/lib.ts
+++ src/lib.ts
@@ -1,1 +1,11 @@
-export default () => "it works";
+import { AST, parse } from "@typescript-eslint/typescript-estree";
+
+// TODO
+export default (vueStr: string): AST<{ jsx: true }> =>
+  parse(
+    `import React from "react";
+
+export default () => <></>;
+`,
+    { jsx: true }
+  );
