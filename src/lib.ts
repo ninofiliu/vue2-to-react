eb9fe7ac88af5985f9783af4cd9f181f210fe85b
Nino Filiu <nino.filiu@gmail.com>
2022-08-09 16:12:57 +0200

    refactor: extract createReactAst

--- src/lib.ts
+++ src/lib.ts
@@ -1,11 +1,65 @@
-import { AST, parse } from "@typescript-eslint/typescript-estree";
+import {
+  AST,
+  AST_NODE_TYPES,
+  parse,
+} from "@typescript-eslint/typescript-estree";
 import { parseComponent } from "vue-template-compiler";
+import { ArrowFunctionExpression } from ".pnpm/@typescript-eslint+types@5.32.0/node_modules/@typescript-eslint/types/dist/generated/ast-spec";
+
+const createReactAst = (
+  params: ArrowFunctionExpression["params"],
+  body: ArrowFunctionExpression["body"]
+): AST<{ jsx: true }> => ({
+  body: [
+    // import React from "react";
+    {
+      assertions: [],
+      importKind: "value",
+      // @ts-ignore
+      source: {
+        raw: '"react"',
+        type: AST_NODE_TYPES.Literal,
+        value: "react",
+      },
+      specifiers: [
+        {
+          // @ts-ignore
+          local: { name: "React", type: AST_NODE_TYPES.Identifier },
+          type: AST_NODE_TYPES.ImportDefaultSpecifier,
+        },
+      ],
+      type: AST_NODE_TYPES.ImportDeclaration,
+    },
+    // export default (PARAMS) => BODY
+    {
+      // @ts-ignore
+      declaration: {
+        async: false,
+        body,
+        expression: true,
+        generator: false,
+        id: null,
+        params,
+        type: AST_NODE_TYPES.ArrowFunctionExpression,
+      },
+      exportKind: "value",
+      type: AST_NODE_TYPES.ExportDefaultDeclaration,
+    },
+  ],
+  sourceType: "module",
+  type: AST_NODE_TYPES.Program,
+});
 
 export default (vueStr: string): AST<{ jsx: true }> => {
   const scfDescriptor = parseComponent(vueStr);
   if (!scfDescriptor.template) {
-    return parse(`import React from "react";export default () => <></>;`, {
-      jsx: true,
+    return createReactAst([], {
+      children: [],
+      // @ts-ignore
+      closingFragment: { type: AST_NODE_TYPES.JSXClosingFragment },
+      // @ts-ignore
+      openingFragment: { type: AST_NODE_TYPES.JSXOpeningFragment },
+      type: AST_NODE_TYPES.JSXFragment,
     });
   }
 };
