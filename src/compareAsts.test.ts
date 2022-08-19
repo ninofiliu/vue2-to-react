3cea9b0279dc91e366b3fb3f5129e8ad4f26e21e
Nino Filiu <nino.filiu@gmail.com>
2022-08-20 01:40:07 +0200

    compare asts

---
+++ src/compareAsts.test.ts
@@ -0,0 +1,122 @@
+import { parse } from "@babel/parser";
+import compareAsts from "./compareAsts";
+
+describe("equivalent ASTs", () => {
+  it("strictly equal", () => {
+    expect(
+      compareAsts(
+        parse(`const elt = <div/>`, { plugins: ["jsx", "typescript"] }),
+        parse(`const elt = <div/>`, { plugins: ["jsx", "typescript"] }),
+        []
+      )
+    ).toEqual({ equal: true });
+  });
+  it("differs by whitespace", () => {
+    expect(
+      compareAsts(
+        parse(`const elt = <div>hello</div>`, {
+          plugins: ["jsx", "typescript"],
+        }),
+        parse(
+          `const elt = <div>
+            hello
+          </div>`,
+          { plugins: ["jsx", "typescript"] }
+        ),
+        []
+      )
+    );
+  });
+  it("differs by parenthesis", () => {
+    expect(
+      compareAsts(
+        parse(`const elt = <div>hello</div>`, {
+          plugins: ["jsx", "typescript"],
+        }),
+        parse(`const elt = (<div>hello</div>)`, {
+          plugins: ["jsx", "typescript"],
+        }),
+        []
+      )
+    );
+  });
+});
+describe("different ASTs", () => {
+  it("differs by text content", () => {
+    expect(
+      compareAsts(
+        parse("<div>hello</div>", { plugins: ["jsx", "typescript"] }),
+        parse("<div>bye</div>", { plugins: ["jsx", "typescript"] }),
+        []
+      )
+    ).toEqual({
+      a: "hello",
+      b: "bye",
+      equal: false,
+      path: [
+        "program",
+        "body",
+        "0",
+        "expression",
+        "children",
+        "0",
+        "extra",
+        "rawValue",
+      ],
+      reason: "Primitives are not equal",
+    });
+  });
+  it("differs by tag name", () => {
+    expect(
+      compareAsts(
+        parse("<h1>hello</h1>", { plugins: ["jsx", "typescript"] }),
+        parse("<h2>hello</h2>", { plugins: ["jsx", "typescript"] }),
+        []
+      )
+    ).toEqual({
+      a: "h1",
+      b: "h2",
+      equal: false,
+      path: [
+        "program",
+        "body",
+        "0",
+        "expression",
+        "openingElement",
+        "name",
+        "name",
+      ],
+      reason: "Primitives are not equal",
+    });
+  });
+  it("differs by attribute", () => {
+    expect(
+      compareAsts(
+        parse(`<div class="hello">world</div>`, {
+          plugins: ["jsx", "typescript"],
+        }),
+        parse(`<div class="bye">world</div>`, {
+          plugins: ["jsx", "typescript"],
+        }),
+        []
+      )
+    ).toEqual({
+      a: "hello",
+      b: "bye",
+      equal: false,
+      path: [
+        "program",
+        "body",
+        "0",
+        "expression",
+        "openingElement",
+        "attributes",
+        "0",
+        "value",
+        "extra",
+        "rawValue",
+      ],
+      reason: "Primitives are not equal",
+    });
+  });
+});
---
+++ src/compareAsts.ts
@@ -0,0 +1,83 @@
+const nodeIsUseless = (node: unknown) =>
+  node !== null &&
+  typeof node === "object" &&
+  "type" in node &&
+  // @ts-ignore
+  node.type === "JSXText" &&
+  // @ts-ignore
+  node.value.trim() === "";
+
+type RecResult =
+  | { equal: true }
+  | {
+      equal: false;
+      a: unknown;
+      b: unknown;
+      path: string[];
+      reason: string;
+    };
+
+const compareAsts = (a: unknown, b: unknown, path: string[]): RecResult => {
+  if (a === null || typeof a !== "object") {
+    if (a !== b) {
+      if (
+        typeof a === "string" &&
+        typeof b === "string" &&
+        a.trim().replaceAll(/\s+/g, " ") === b.trim().replaceAll(/\s+/g, " ")
+      ) {
+        return { equal: true };
+      }
+      return { equal: false, a, b, path, reason: "Primitives are not equal" };
+    }
+    return { equal: true };
+  }
+  if (b === null || typeof b !== "object") {
+    return { equal: false, a, b, path, reason: "a is an object but b is not" };
+  }
+
+  for (const c of [a, b]) {
+    if (nodeIsUseless(c)) return { equal: true };
+  }
+
+  if (Array.isArray(a)) {
+    if (!Array.isArray(b)) {
+      return { equal: false, a, b, path, reason: "a is an array but b is not" };
+    }
+    const aChildrenToCompare = a.filter((node) => !nodeIsUseless(node));
+    const bChildrenToCompare = b.filter((node) => !nodeIsUseless(node));
+    if (aChildrenToCompare.length !== bChildrenToCompare.length) {
+      return {
+        equal: false,
+        a,
+        b,
+        path,
+        reason: "a and b do not have the same number of useful children",
+      };
+    }
+    for (let i = 0; i < aChildrenToCompare.length; i++) {
+      const childResult = compareAsts(
+        aChildrenToCompare[i],
+        bChildrenToCompare[i],
+        [...path, `${a.indexOf(aChildrenToCompare[i])}`]
+      );
+      if (!childResult.equal) return childResult;
+    }
+  } else {
+    const keys = new Set([...Object.keys(a), ...Object.keys(b)]);
+    keys.delete("start");
+    keys.delete("end");
+    keys.delete("loc");
+    for (const key of keys) {
+      const childResult = compareAsts(
+        a[key as keyof typeof a],
+        b[key as keyof typeof b],
+        [...path, key]
+      );
+      if (!childResult.equal) return childResult;
+    }
+  }
+
+  return { equal: true };
+};
+
+export default compareAsts;
